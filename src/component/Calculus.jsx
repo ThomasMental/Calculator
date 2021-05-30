01d00f744836ac85faed03876bb7241e79c55261
ThomasMental <tommental@163.com>
2021-05-30 16:46:50 +0800

    Update Calculus.jsx

--- src/component/Calculus.jsx
+++ src/component/Calculus.jsx
@@ -7,20 +7,9 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 export default class Calculus extends Component {
   render() {
     return (
-      <Tabs>
-          <TabList>
-            <Tab>Derivative</Tab>
-            <Tab>Integral</Tab>
-          </TabList>
 
-          <TabPanel>
             <Derivative />
-          </TabPanel>
 
-          <TabPanel>
-            <Integral />
-          </TabPanel>
-        </Tabs>
     );
   }
 }
